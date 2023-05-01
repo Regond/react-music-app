1cadf2491bc2eaa36ee04aaf73000978055ab0a6
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 19:18:30 +0300

    add sidebar button component

--- react-musify-app/src/components/sidebar/sidebar.js
+++ react-musify-app/src/components/sidebar/sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './sidebar.module.css';
+import SidebarButton from './sidebarButton';
 
 export default function Sidebar() {
   return (
-    <div className={styles.mainContainer}>Sidebar</div>
+    <div className={styles.mainContainer}>
+        
+    </div>
   )
 }
---
+++ react-musify-app/src/components/sidebar/sidebarButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './sidebarButton.module.css'
+
+export default function SidebarButton() {
+  return (
+    <div>SiberbarBtn</div>
+  )
+}
