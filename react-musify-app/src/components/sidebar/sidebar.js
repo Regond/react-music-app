92bd27efda3b67e73b5d3931eb48a0d32639edec
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 19:24:25 +0300

    add sidebar button component layout using props

--- react-musify-app/src/components/sidebar/sidebar.js
+++ react-musify-app/src/components/sidebar/sidebar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './sidebar.module.css';
 import SidebarButton from './sidebarButton';
+import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   return (
     <div className={styles.mainContainer}>
-        
+        <div>
+          <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        </div>   
     </div>
   )
 }
--- react-musify-app/src/components/sidebar/sidebarButton.js
+++ react-musify-app/src/components/sidebar/sidebarButton.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import styles from './sidebarButton.module.css'
+import { IconContext } from "react-icons";
+import { Link, useLocation } from "react-router-dom";
 
-export default function SidebarButton() {
+export default function SidebarButton(props) {
+
+  const location = useLocation();
+
+  const isActive = location.pathname === props.to;
+  
+  const btnClass = isActive ? "btn-body active" : "btn-body";
   return (
-    <div>SiberbarBtn</div>
+    <Link to={props.to}>
+      <div className={btnClass}>
+        <IconContext.Provider className={styles.btnIcon} value={{ size: "24px" }}>
+          {props.icon}
+          <p className={styles.btnTitle}>{props.title}</p>
+        </IconContext.Provider>
+      </div>
+    </Link>
   )
 }
