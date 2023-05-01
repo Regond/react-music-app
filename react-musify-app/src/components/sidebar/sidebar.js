378f8dc0ac710e836e97acdccb8239dcf95fb4f0
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 20:17:07 +0300

    add user info

--- react-musify-app/src/components/sidebar/sidebar.js
+++ react-musify-app/src/components/sidebar/sidebar.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import styles from './sidebar.module.css';
 import SidebarButton from './sidebarButton';
 import { MdSpaceDashboard } from "react-icons/md";
 import { MdFavorite } from "react-icons/md";
 import { FaGripfire, FaPlay } from "react-icons/fa";
-import { FaSignOutAlt } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 
 export default function Sidebar() {
+
+  const [image, setImage] = useState(
+    <FaUserAlt className={styles.profileImg} />
+  );
+  const [name, setName] = useState('Гість');
   return (
     <div className={styles.mainContainer}>
         <Link to='/' className={styles.Logo}>
@@ -22,6 +27,13 @@ export default function Sidebar() {
           <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />}/>
           <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
         </div>   
+
+        <div className={styles.userInfo}>
+          {image}
+          {/* <p className={styles.userName}>{name}</p> */}
+        </div>
+
+
     </div>
   )
 }
