efb07cf5c9832854885f3b72b3d60bd5fda2ab0e
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 20:26:55 +0300

    connecting data include

---
+++ react-musify-app/src/spotify.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const authEndpoint = 'https://accounts.spotify.com/authorize?',
+      clientID =  '7206f7705f26434b8370f90501806e30' ,
+      redirectUri = 'http://localhost:3000';
