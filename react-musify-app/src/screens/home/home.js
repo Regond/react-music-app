2bd61153d245c84902c82f2f3c47b7ccd86c0550
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 18:38:31 +0300

    add feed component

--- react-musify-app/src/screens/home/home.js
+++ react-musify-app/src/screens/home/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Library from '../library/library';
+import Feed from '../feed/feed'
 export default function Home() {
   return (
     <div>
         <Router>
             <Routes>
             <Route path="/" element={<Library />} />
-
+            <Route path="/feed" element={<Feed />} />
             </Routes>
         </Router>
     </div>
