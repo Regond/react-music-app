acfbea6ff588e5deda8b1229c6b713de96e91ad1
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 20:29:54 +0300

    export client token

--- react-musify-app/src/spotify.js
+++ react-musify-app/src/spotify.js
@@ -13,3 +13,12 @@ export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=
   const apiClient = axios.create({
     baseURL: "https://api.spotify.com/v1/",
   });
+
+  export const setClientToken = (token) => {
+    apiClient.interceptors.request.use(async function (config) {
+      config.headers.Authorization = "Bearer " + token;
+      return config;
+    });
+  };
+  
+  export default apiClient;
