aa11bc6097fa4d3aaea567ca344b52eff5760b43
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 18:43:47 +0300

    add trending component

--- react-musify-app/src/screens/home/home.js
+++ react-musify-app/src/screens/home/home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Library from '../library/library';
 import Feed from '../feed/feed';
-import Favorites from '../favorites/favorites'
+import Favorites from '../favorites/favorites';
+import Trending from '../trending/trending';
+import Player from '../player/player';
 export default function Home() {
   return (
     <div>
@@ -10,7 +12,9 @@ export default function Home() {
             <Routes>
                 <Route path="/" element={<Library />} />
                 <Route path="/feed" element={<Feed />} />
+                <Route path="/trending" element={<Trending />} />
                 <Route path="/favorites" element={<Favorites />} />
+                <Route path="/player" element={<Player />} />
             </Routes>
         </Router>
     </div>
