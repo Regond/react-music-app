8f5f55b96f8fadeb1912e04798748b7c6e49f19a
Vasya Tkach <vasatkach8@gmail.com>
2023-05-01 18:41:22 +0300

    add favorites component

--- react-musify-app/src/screens/home/home.js
+++ react-musify-app/src/screens/home/home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Library from '../library/library';
-import Feed from '../feed/feed'
+import Feed from '../feed/feed';
+import Favorites from '../favorites/favorites'
 export default function Home() {
   return (
     <div>
         <Router>
             <Routes>
-            <Route path="/" element={<Library />} />
-            <Route path="/feed" element={<Feed />} />
+                <Route path="/" element={<Library />} />
+                <Route path="/feed" element={<Feed />} />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </Router>
     </div>
