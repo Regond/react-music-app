33370d1e11f5db60b4d26eaeeb1a51b648478a93
Vasya Tkach <vasatkach8@gmail.com>
2023-05-02 21:36:45 +0300

    add header for favorites

--- react-musify-app/src/screens/favorites/favorites.js
+++ react-musify-app/src/screens/favorites/favorites.js
@@ -3,7 +3,8 @@ import { useEffect } from 'react';
 import APIKit from '../../spotify';
 import styles from './favorites.module.css';
 import Song from './song';
-
+import { IconContext } from "react-icons";
+import { MdFavorite } from "react-icons/md";
 export default function Favorites() {
 
   const [tracks, setTracks] = useState(null);
@@ -17,7 +18,12 @@ export default function Favorites() {
  
   return (
     <div className={styles.main}>
-      <h1 className={styles.mainTitle}>Your favorites</h1>
+      <div className={styles.favoritesHeader}>
+        <IconContext.Provider value={{ size: "100px", color: "#fff" }}>
+            <MdFavorite />
+        </IconContext.Provider>
+        <h1 className={styles.mainTitle}>Your favorites</h1>
+      </div>
       {tracks?.map((track) => (
         <Song track = {track.track} key = {track.track.id}/>
       ))}
