c2637cb1d24b63e3c8548a37fd325960e0189a6d
Vasya Tkach <vasatkach8@gmail.com>
2023-05-02 20:00:23 +0300

    fetch favorites from api

--- react-musify-app/src/screens/favorites/favorites.js
+++ react-musify-app/src/screens/favorites/favorites.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useEffect } from 'react';
+import APIKit from '../../spotify';
 
 export default function Favorites() {
+
+  const [tracks, setTracks] = useState(null);
+
+  useEffect(() => {
+      APIKit.get('me/tracks').then(function(response) {
+        setTracks(response.data.items);
+    })
+  }, []) 
+
+
   return (
-    <div>Favorites</div>
+    <div> </div>
   )
 }
