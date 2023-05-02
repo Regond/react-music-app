11d40dd54b365ae9d34f544c372eaac9ed9c03e8
Vasya Tkach <vasatkach8@gmail.com>
2023-05-02 21:49:32 +0300

    add user profile picture and name to header

--- react-musify-app/src/screens/favorites/favorites.js
+++ react-musify-app/src/screens/favorites/favorites.js
@@ -9,20 +9,41 @@ export default function Favorites() {
 
   const [tracks, setTracks] = useState(null);
 
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+
   useEffect(() => {
       APIKit.get('me/tracks').then(function(response) {
         setTracks(response.data.items);
     })
   }, []) 
 
+  useEffect(() => {
+    APIKit.get('me/').then(function(response) {
+      setImage(response.data.images[0].url);
+      setName(response.data.display_name);
+  })
+}, [])
+
  
   return (
     <div className={styles.main}>
       <div className={styles.favoritesHeader}>
-        <IconContext.Provider value={{ size: "100px", color: "#fff" }}>
+        <IconContext.Provider value={{ size: "150px", color: "#DEDFE4" }}>
             <MdFavorite />
         </IconContext.Provider>
-        <h1 className={styles.mainTitle}>Your favorites</h1>
+        <div className={styles.info}>
+          <h1 className={styles.mainTitle}>Your favorites</h1>
+          <div className={styles.userInfo}>
+            <img 
+              src={image}
+              alt={name}
+              className={styles.profilePic}
+            />
+            <h2 className={styles.name}>{name}</h2>
+          </div>
+        </div>
+
       </div>
       {tracks?.map((track) => (
         <Song track = {track.track} key = {track.track.id}/>
