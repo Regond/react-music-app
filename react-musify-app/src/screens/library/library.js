d06b13acd99234654609978798b5f8d77c87fa0c
Vasya Tkach <vasatkach8@gmail.com>
2023-05-02 13:59:34 +0300

    fetch playlist from api

--- react-musify-app/src/screens/library/library.js
+++ react-musify-app/src/screens/library/library.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useEffect } from 'react';
+import APIKit from '../../spotify';
+import { IconContext } from "react-icons";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import styles from './library.module.css';
 
 export default function Library() {
+  const [playlists, setPlaylists] = useState(null)
+
+  useEffect(() => {
+      APIKit.get('me/playlists').then(function(response) {
+      setPlaylists(response.data.items)
+    })
+  }, []) 
+  
   return (
     <div>library</div>
   )
