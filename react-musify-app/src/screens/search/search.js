adda7bf36aef519313cc6ca963a4f16f43bb9e1d
Vasya Tkach <vasatkach8@gmail.com>
2023-05-03 18:16:56 +0300

    rename component function

--- react-musify-app/src/screens/search/search.js
+++ react-musify-app/src/screens/search/search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import APIKit from '../../spotify';
 
 
-export default function Feed() {
+export default function Search() {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
