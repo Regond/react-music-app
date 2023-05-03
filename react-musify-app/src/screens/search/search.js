ca192e4376fc8967ca127192e426c9b978c5354f
Vasya Tkach <vasatkach8@gmail.com>
2023-05-03 17:40:43 +0300

    add states for search items

--- react-musify-app/src/screens/search/search.js
+++ react-musify-app/src/screens/search/search.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import APIKit from '../../spotify';
+
 
 export default function Feed() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  
   return (
     <div>Search</div>
   )
