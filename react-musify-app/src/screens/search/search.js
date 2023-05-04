c7f626bbe2ec7da9db9ff31046adc4b9049f1ee4
Vasya Tkach <vasatkach8@gmail.com>
2023-05-04 18:43:40 +0300

    style search bar

--- react-musify-app/src/screens/search/search.js
+++ react-musify-app/src/screens/search/search.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Recomendations from '../../components/recomendations/recomendations';
 import APIKit from '../../spotify';
 import styles from './search.module.css';
+import { IoSearch } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
 export default function Search() {
 
@@ -33,9 +35,16 @@ export default function Search() {
     <div className={styles.mainContainer}>
 
       <div className={styles.searchContainer}>
-        <form onSubmit={handleFormSubmit}>
-          <input type="text" value={searchTerm} className={styles.searchInput} onChange={handleInputChange} />
-          <button type="submit">Search</button>
+        <form className={styles.form} onSubmit={handleFormSubmit}>
+          <div className={styles.inputContainer}>
+            <IconContext.Provider value={{ size: "32px" }}>
+              <IoSearch />
+            </IconContext.Provider>
+
+            <input type="text" placeholder='Enter the author or track' value={searchTerm} className={styles.searchInput} onChange={handleInputChange} />
+            <button className={styles.searchButton} type="submit">Search</button>
+         </div>
+
         </form>
         <ul>
           {searchResults.map((track) => (
