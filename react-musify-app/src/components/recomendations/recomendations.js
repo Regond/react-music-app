aeb77a6492e05188da57fdd0bdbb6f1d357ae625
Vasya Tkach <vasatkach8@gmail.com>
2023-05-04 20:49:16 +0300

    fetch new releases from api

--- react-musify-app/src/components/recomendations/recomendations.js
+++ react-musify-app/src/components/recomendations/recomendations.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import styles from './recomendation.module.css';
+import APIKit from '../../spotify';
+import Song from '../../screens/favorites/song';
+
 
 export default function Recomendations() {
+
+  const [tracks, setTracks] = useState(null);
+
+  useEffect(() => {
+    APIKit.get('browse/new-releases').then(function(response) {
+      setTracks(response.data.albums.items);
+      console.log(response.data.albums.items);
+  })
+}, []) 
+
+
+
   return (
-    <div>Recomendations</div>
+    <div className={styles.mainContainer}>
+      Recomendations
+    </div>
   )
 }
