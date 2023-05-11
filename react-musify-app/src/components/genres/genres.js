29bb0fbcd664d25f1538817aeb1fd132c69566e9
Vasya Tkach <vasatkach8@gmail.com>
2023-05-11 11:57:38 +0300

    add fetch genres from api

--- react-musify-app/src/components/genres/genres.js
+++ react-musify-app/src/components/genres/genres.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './genres.module.css';
+import APIKit from '../../spotify';
 
 export default function Genres() {
+
+    const [genres, setGenres] = useState(null);
+
+  useEffect(() => {
+    APIKit.get('recommendations/available-genre-seeds').then(function(response) {
+      setGenres(response.data.genres);
+      console.log(response.data.genres);
+  })
+}, []) 
   return (
     <div>Genres</div>
   )
