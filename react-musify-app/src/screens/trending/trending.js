1752ef99428ae17ca47603f2fdfc14010b9c2bf9
Vasya Tkach <vasatkach8@gmail.com>
2023-05-11 17:45:46 +0300

    add fetch playlists sort by genre

--- react-musify-app/src/screens/trending/trending.js
+++ react-musify-app/src/screens/trending/trending.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import GenreList from '../../components/genreList/genreList';
 
 export default function Trending() {
   const location = useLocation();
   const showGenreItems =  location.state?.genre;
   return (
     !showGenreItems ? 
-    <div>trending</div> : location.state?.genre
+      <></> 
+      
+      : 
+    <GenreList genre = {showGenreItems}/>
   )
 }
