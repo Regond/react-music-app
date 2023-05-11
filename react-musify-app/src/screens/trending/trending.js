070d4a82e6699e3bcfe86454049401c6f0991639
Vasya Tkach <vasatkach8@gmail.com>
2023-05-11 17:21:49 +0300

    add redirect by genre

--- react-musify-app/src/screens/trending/trending.js
+++ react-musify-app/src/screens/trending/trending.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 export default function Trending() {
+  const location = useLocation();
+  const showGenreItems =  location.state?.genre;
   return (
-    <div>trending</div>
+    !showGenreItems ? 
+    <div>trending</div> : location.state?.genre
   )
 }
