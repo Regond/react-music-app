b9b825f96965f2bf85c0f2e18d530bf679068242
Vasya Tkach <vasatkach8@gmail.com>
2023-05-16 16:27:57 +0300

    set tracks from location state

--- react-musify-app/src/screens/player/player.js
+++ react-musify-app/src/screens/player/player.js
@@ -9,6 +9,18 @@ export default function Player() {
   const [tracks, setTracks] = useState([]);
   const [currentTrack, setCurrentTrack] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if(location.state) {
+      apiClient
+      .get("playlists/" + location.state?.id + "/tracks")
+      .then(response => {
+        setTracks(response.data.items);
+        setCurrentTrack(response.data?.items[0]?.track);
+      });
+    }
+  }, [location.state]);
+
   
   return (
     <div className={styles.mainContainer}>
