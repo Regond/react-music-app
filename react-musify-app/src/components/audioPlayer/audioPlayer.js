b72f0f36f9f78ce60c3c90d79fa203beacfef391
Vasya Tkach <vasatkach8@gmail.com>
2023-05-16 17:35:03 +0300

    add song title type and duration

--- react-musify-app/src/components/audioPlayer/audioPlayer.js
+++ react-musify-app/src/components/audioPlayer/audioPlayer.js
@@ -21,7 +21,9 @@ export default function AudioPlayer({
                 src={currentTrack?.album?.images[0]?.url}
             />
             <div className={styles.info}>
-
+                <h1 className={styles.songTitle}>{currentTrack?.name} - {artists.join(" | ")}</h1>
+                <h2 className={styles.duration}>Duration: 0:30</h2>
+                <div className={styles.type}>{currentTrack?.type}</div>
             </div>
         </div>
         <div className={styles.controls}>
