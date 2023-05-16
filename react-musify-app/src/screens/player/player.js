3ebd571772a2c5e3c756763275541842a0b04048
Vasya Tkach <vasatkach8@gmail.com>
2023-05-16 16:26:59 +0300

    add states for track

--- react-musify-app/src/screens/player/player.js
+++ react-musify-app/src/screens/player/player.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './player.module.css';
+import { useLocation } from "react-router-dom";
+import APIKit from "../../spotify";
 
-export default function layer() {
+export default function Player() {
+
+  const location = useLocation();
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
   return (
     <div className={styles.mainContainer}>
-        Hello
     </div>
   )
 }
